be899ae83e8c0455d5e91d54ce0453293fba2988
Paola <Paola@DESKTOP-9IIFPRA>
2023-07-26 11:41:39 -0400

    Agregar archivos de proyecto.

---
+++ apppp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace apppp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
